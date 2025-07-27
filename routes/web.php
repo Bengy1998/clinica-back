4ebe0ac2dc2438cf58508e19b845f4d063439e7a
bjar <bengyjar1998@outlook.com>
2025-07-27 13:43:57 -0500

    clinica

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/auth/google', [UserAuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [UserAuthController::class, 'handleGoogleCallback']);
 //php artisan jwt:secret
+Route::get('/', function () {
+    return view('welcome');
+});
+//php artisan jwt:secret
+// Ruta comodín para manejar las rutas de Vue.js
+Route::get('/{any}', function () {
+    return view('welcome'); // Asegúrate de que esta vista cargue tu compilado de Vue.js
+})->where('any', '.*');
